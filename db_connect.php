8f4862bef062b473bf8309fc2a72cbc610b1c09c
Colin Greenwell <105884295+Clo612@users.noreply.github.com>
2024-04-19 12:23:18 -0500

    Create db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$database = "user_auth";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
